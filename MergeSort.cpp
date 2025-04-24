474b7bf35b6a611632e2bd878e060ab37f332a9a
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-04-24 10:51:54 +0700

    membuat cout

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -19,4 +19,7 @@ void input()
             cout << "\nMaksimal Panjang array adalah 20";
         }
     }
+    cout << "\n-------------------------" << endl;
+    cout << "\nInputkan Isi element array" << endl;
+    cout << "\n-------------------------" << endl;
 }
